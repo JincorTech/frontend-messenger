079c29f9bcdbf1896a467fad5a4f66c481eafcf4
David Totraev <totraev@gmail.com>
2017-05-23 11:34:28 +0300

    init

---
+++ postcss.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: {
+    'postcss-use': {
+      modules: [
+        'postcss-short',
+        'postcss-inline-svg',
+        'postcss-center',
+        'postcss-clearfix',
+        'postcss-triangle'
+      ]
+    },
+    'postcss-cssnext': {},
+    'postcss-assets': {
+      relative: true
+    },
+    'doiuse': {
+      browsers: ['ie >= 10', '> 5%']
+    },
+    'postcss-reporter': {
+      clearAllMessages: true
+    }
+  }
+}
---
+++ src/redux/rootReducer.ts
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+/**
+ * Global App state
+ */
+export type State = {};
+
+/**
+ * Root reducer
+ */
+export default combineReducers<State>({});
