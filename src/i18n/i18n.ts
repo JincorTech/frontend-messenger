12bd62bd802f0121044d7f2c73cb69d94b14e920
Alexander Isaev <aleksandr.isaev@akvelon.com>
2017-10-02 13:47:41 +0400

    i18next library was added

---
+++ src/i18n/i18n.ts
@@ -0,0 +1,41 @@
+import * as i18n from 'i18next';
+
+const ru = {
+  app: require('../locales/ru/app.json')
+};
+
+const en = {
+  app: require('../locales/en/app.json')
+};
+
+export const config = {
+  lng: 'en',
+  fallbackLng: 'en',
+  supportedLngs: ['en', 'ru'],
+  debug: process.env.NODE_ENV === 'development',
+  react: {
+    wait: true,
+    nsMode: 'default'
+  },
+  resources: {
+    en: {
+      app: en.app
+    },
+    ru: {
+      app: ru.app
+    }
+  },
+  ns: ['common'],
+  defaultNS: 'common',
+  detection: {
+    order: ['querystring', 'localStorage'],
+    lookupQuerystring: 'lang',
+    lookupLocalStorage: 'i18nextLng',
+    caches: ['localStorage']
+  }
+};
+
+const instance = i18n
+  .init(config);
+
+export default instance;
