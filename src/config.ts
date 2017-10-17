81fd6586ac3b2e4de62e2bfd1b8ac7a2155b7a3d
Andrey Degtyaruk <hlogeon1@gmail.com>
2017-10-18 05:02:05 +0700

    Change the config

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ const { API_HOST, API_PREFIX, NODE_ENV } = process.env;
 
 export default {
   apiPrefix: API_PREFIX || '/api/v1',
-  apiHost: NODE_ENV === 'production'
-    ? API_HOST || 'http://localhost:8080'
-    : 'http://139.162.132.212:8080'
+  apiHost: NODE_ENV = API_HOST
+//    ? API_HOST || 'http://localhost:8080'
+//    : 'http://139.162.132.212:8080'
 };
 
 export const messengerConfig = {
